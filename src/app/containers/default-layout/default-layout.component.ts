0f134a08585a6a9ed6f0ea449e127b4ad617d076
Abdenaji Chiba <abdenajichiba12@gmail.com>
2022-08-18 17:29:40 +0100

    round diff in carburant analysis

--- src/app/containers/default-layout/default-layout.component.ts
+++ src/app/containers/default-layout/default-layout.component.ts
@@ -69,6 +69,9 @@ export class DefaultLayoutComponent implements AfterViewInit, OnInit, OnDestroy 
               if (this.notifregx.z3.test(e)) {
                 notifsTmp.z++
               }
+              if (this.notifregx.d.test(e)) {
+                notifsTmp.d++
+              }
               if (this.notifregx.f.test(e)) {
                 notifsTmp.f++
               }
